d58d5d8e484733c4932694c780b05793cdfbbdaf
ManasPatil0967 <manaspatil0967@gmail.com>
2024-02-17 14:57:32 +0530

    Update 2:57 PM

--- dashboard/firebase/db.js
+++ dashboard/firebase/db.js
@@ -5,6 +5,8 @@ export const getUser = async (uid) => {
     try {
         const userDoc = doc(db, "users", uid);
         const userSnap = await getDoc(userDoc);
+        console.log("Get Doc triggered");
+        console.log(userSnap.data());
         return userSnap.data();
     } catch (error) {
         return error;
--- dashboard/pages/profile.js
+++ dashboard/pages/profile.js
@@ -1,19 +1,25 @@
 import { Navbar } from "@/components/Navbar";
 import { getUser } from "@/firebase/db";
+import { db } from "@/firebase/config";
+import { doc, getDoc } from "firebase/firestore"
 import { onAuthStateChanged } from "@/firebase/auth";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 export default function Profile() {
     const [user, setUser] = useState(null);
+    const [userData, setUserData] = useState(null);
     const router = useRouter();
     useEffect(() => {
-        onAuthStateChanged((user) => {
+        onAuthStateChanged(async (user) => {
             if (user) {
-                getUser(user.uid).then((userData) => {
-                    console.log("userData: ",userData);
-                    setUser(userData);
-                });
+                setUser(user);
+                console.log(user);
+                const docRef = doc(db, "users", user.uid);
+                const userSnap = await getDoc(docRef);
+                console.log("Get Doc triggered in Profile.js");
+                console.log(userSnap.data());
+                setUserData(userSnap.data());
             } else {
                 router.push("/login");
             }
