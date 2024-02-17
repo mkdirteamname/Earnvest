48de46020852c7c99ba3f7739933772df9ee5e41
ManasPatil0967 <manaspatil0967@gmail.com>
2024-02-17 19:10:35 +0530

    Asset charts added

--- dashboard/pages/assets/[asset].js
+++ dashboard/pages/assets/[asset].js
@@ -1,6 +1,7 @@
 import { Navbar } from "@/components/Navbar";
+import Stocks from "@/components/Stocks";
 import { useRouter } from "next/router";
-import { useEffect } from "react";
+import { useState } from "react";
 
 export default function Assets() {
   const router = useRouter();
@@ -10,23 +11,27 @@ export default function Assets() {
   return (
         <div>
       <Navbar />
-            <p>Asset : {asset}</p>
       {asset === "gold" ? (
-        <div>
-          <p>Gold</p>
+        <div className="flex justify-center text-center">
+          <p className="bold text-xl">Gold</p>
+          <Stocks ticker="XAUUSD" />
           </div>
         ) : asset === "bonds" ? (
             <div>
                 <p>Bonds</p>
+                <Stocks ticker="US10Y" />
+                <Stocks ticker="IN10Y" />
             </div>
             ) : asset === "stocks" ? (
             <div>
                 <p>Stocks</p>
-                <input type="text" value={ticker} onChange={(event) => setEmail(event.currentTarget.value)} />
+                <input type="text" value={ticker} onChange={(event) => setTicker(event.currentTarget.value)} />
+                <Stocks ticker={ticker} />
             </div>
             ) : asset === "mfs" ? (
             <div>
                 <p>Mutual Funds</p>
+                <Stocks ticker="TVF2G" />
             </div>
             ) : (
             <div>
