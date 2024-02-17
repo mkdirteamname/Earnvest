ca18a376f37bac71e1f7068ec3c38a1862d13c56
malharrrr <malharbonde12@gmail.com>
2024-02-17 09:51:27 +0530

    firebase.js added

---
+++ portfolio-management/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDeZNyxv9e6fXEBTQzmwuJPz2E6SvA1RPg",
+  authDomain: "portfolio-management-93402.firebaseapp.com",
+  projectId: "portfolio-management-93402",
+  storageBucket: "portfolio-management-93402.appspot.com",
+  messagingSenderId: "959390801965",
+  appId: "1:959390801965:web:0720f0bedf849de90b58a4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
